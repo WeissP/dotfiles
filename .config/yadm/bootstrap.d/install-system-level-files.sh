c80540433ebec57e8d7f693343563c551aa6a069
weiss <kingbaiing@163.com>
2021-06-20 18:01:11 +0200

    add all dotfiles

---
+++ .config/yadm/bootstrap.d/install-system-level-files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo rsync -aArXvIP ~/.config/system-level-dotfiles/* /
+
+
---
+++ weiss/AutoStart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# python ~/Python/hotstrings.py&
+unclutter --exclude-root --ignore-scrolling&
+/usr/lib/gsd-xsettings&
+/opt/apps/com.qq.office.deepin/files/run.sh
+/opt/deepinwine/apps/Deepin-WeChat/run.sh -u %u
+# /opt/deepinwine/apps/Deepin-TIM/run.sh&
+# /opt/deepinwine/apps/Deepin-WeChat/run.sh&
+# ~/.screenlayout/DualScreen.sh&
+# xrandr --output eDP1 --mode 3000x2000 --pos 0x0 --scale 0.55x0.55 --rotate normal --output DP1 --primary --mode 1920x1080 --pos 1650x0 --rotate normal --output DP2 --off --output VIRTUAL1 --off &
+# xbindkeys -f ~/.xbindkeysrc&
+mouse.sh&
+emacs --dump-file="/home/weiss/.emacs.d/emacs.pdmp"
+ # sslocal -c ~/.config/shadowsocks/config.json && # mountavf # xset r rate 230 30 

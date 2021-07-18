1a383c3a42862722c56b98491d4cfd600731aba3
weiss <kingbaiing@163.com>
2021-07-18 21:12:25 +0200

    update mouse.sh

--- weiss/mouse.sh
+++ weiss/mouse.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-Mouse_ID=$(xinput list | grep "ELECOM ELECOM TrackBall Mouse" | head -n 1 | sed -r 's/.*id=([0-9]+).*/\1/')
-xinput set-prop ${Mouse_ID} 'libinput Button Scrolling Button' 9
-xinput set-prop ${Mouse_ID} "libinput Scroll Method Enabled" 0 0 1
+Mouse_name="ELECOM ELECOM TrackBall Mouse"
+
+Mouse_ID=$(xinput list | grep "$Mouse_name" | head -n 1 | sed -r 's/.*id=([0-9]+).*/\1/')
+if [ -z "$Mouse_ID" ]; then
+    echo "ELECOM TrackBall Mouse not found"
+else
+    xinput set-prop ${Mouse_ID} 'libinput Button Scrolling Button' 9
+    xinput set-prop ${Mouse_ID} "libinput Scroll Method Enabled" 0 0 1
+fi

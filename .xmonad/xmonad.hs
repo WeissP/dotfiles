0ca4db50beaf7c07b6e80c9262644df2ffe14b76
weiss <kingbaiing@163.com>
2021-07-20 23:41:04 +0200

    update xmonad config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -88,6 +88,127 @@ myFocusedBorderColor = "#aa0000"
 --   time <- currentTime
 --   evt  <-
 
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+ 
+    -- launch a terminal
+    [ ((<F6>)       , spawn $ XMonad.terminal conf)
+    , ((<F17>)            , spawn "dmenu_run")
+ 
+--     , ((modMask,               xK_p     )       , spawn "mpc toggle")
+--     , ((modMask,               xK_bracketright) , spawn "mpc volume +5")
+--     , ((modMask,               xK_bracketleft)  , spawn "mpc volume -5")
+--     , ((modMask .|. shiftMask, xK_l)            , spawn "gnome-screensaver-command --lock")
+
+--     , ((modMask              , xK_x)            , spawn "xmodmap ~/.Xmodmap")
+ 
+--     -- close focused window 
+--     , ((modMask .|. shiftMask, xK_c     ), kill)
+ 
+--      -- Rotate through the available layout algorithms
+--     , ((modMask,               xK_space ), sendMessage NextLayout)
+ 
+--     --  Reset the layouts on the current workspace to default
+--     , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+ 
+--     -- Resize viewed windows to the correct size
+--     , ((modMask,               xK_n     ), refresh)
+ 
+--     -- Move focus to the next window
+--     , ((modMask,               xK_Tab   ), windows W.focusDown)
+ 
+--     -- Move focus to the next window
+--     , ((modMask,               xK_j     ), windows W.focusDown)
+ 
+--     -- Move focus to the previous window
+--     , ((modMask,               xK_k     ), windows W.focusUp  )
+ 
+--     -- Move focus to the master window
+--     , ((modMask,               xK_m     ), windows W.focusMaster  )
+ 
+--     -- Swap the focused window and the master window
+--     , ((modMask,               xK_Return), windows W.swapMaster)
+ 
+--     -- Swap the focused window with the next window
+--     , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  )
+ 
+--     -- Swap the focused window with the previous window
+--     , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    )
+ 
+--     -- Shrink the master area
+--     , ((modMask,               xK_h     ), sendMessage Shrink)
+ 
+--     -- Expand the master area
+--     , ((modMask,               xK_l     ), sendMessage Expand)
+ 
+--     -- Push window back into tiling
+--     , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
+ 
+--     -- Increment the number of windows in the master area
+--     , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
+ 
+--     -- Deincrement the number of windows in the master area
+--     , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
+ 
+--     -- toggle the status bar gap
+--     , ((modMask              , xK_b     ), sendMessage ToggleStruts)
+ 
+--     -- Restart xmonad
+--     , ((modMask .|. shiftMask, xK_r     ),
+--        broadcastMessage ReleaseResources >> restart "xmonad" True)
+
+--     , ((modMask              , xK_g     ),
+--        workspacePrompt  myXPConfig (windows . W.greedyView))
+--     , ((modMask .|. shiftMask, xK_g     ),
+--        workspacePrompt  myXPConfig (windows . W.shift))
+
+--     -- C-t submap
+--     , ((controlMask, xK_t)       , submap . M.fromList $
+--        [ ((controlMask,  xK_t)      ,   toggleWS) 
+--        , ((0,            xK_Tab)    ,   windows W.focusDown) -- @@ Move focus to the next window
+--        , ((shiftMask,    xK_Tab)    ,   windows W.focusUp  ) -- @@ Move focus to the previous window
+--        , ((0,            xK_c)      ,   spawn $ XMonad.terminal conf)
+--        , ((0,            xK_k)      ,   kill)
+--        , ((0,            xK_Return) ,   windows W.swapMaster)
+-- --       , ((shiftMask,    xK_1)      ,   spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+--        , ((shiftMask,    xK_1)      ,   scratchpadSpawnActionTerminal "gterm")
+--        , ((0,            xK_p)      ,   spawn "/home/nelhage/bin/viewpdf")
+--        , ((0,            xK_s)      ,   sshPrompt myXPConfig)
+
+--        , ((shiftMask,    xK_s)      ,   spawn "/usr/bin/tracker-search-tool")
+--        , ((0,            xK_x)      ,   spawn "/home/nelhage/bin/rp-hm-complete.sh")
+
+--        , ((0,            xK_g)      ,   workspacePrompt  myXPConfig (windows . W.greedyView))
+--        , ((shiftMask,    xK_g)      ,   workspacePrompt  myXPConfig (windows . W.shift))
+--        , ((0,            xK_n)      ,   inputPrompt myXPConfig "Workspace name" ?+ addWorkspace)
+--        , ((shiftMask,    xK_k)      ,   removeWorkspace)
+
+--        , ((0,            xK_r)      ,   renameWorkspace myXPConfig)
+--        , ((0,            xK_q)      ,   viewScreen 0)
+--        , ((0,            xK_w)      ,   viewScreen 1)
+--        ] ++
+--        [((0, k), windows $ W.greedyView i)
+--         | (i, k) <- zip (take 10 (XMonad.workspaces conf)) [xK_1 ..]
+--        ])
+    ]
+    -- ++
+ 
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    -- [((m .|. modMask, k), windows $ f i)
+        -- | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        -- , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    -- ++
+ 
+    --
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    --
+    -- [((modMask .|. mask, key), f sc)
+        -- | (key, sc) <- zip [xK_q, xK_w, xK_e] [0..]
+        -- , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]]
+
 
 -- renameWS :: String -> X ()
 -- renameWS newTag = windows $ \s -> let old = W.tag $ W.workspace $ W.current s
@@ -109,7 +230,7 @@ defaults = defaultConfig {
       -- simple stuff
         terminal           = myTerminal,
         -- borderWidth        = myBorderWidth,
-        modMask            = myModMask
+        modMask            = myModMask,
         -- numlockMask        = myNumlockMask,
         -- workspaces         = myWorkspaces,
         -- normalBorderColor  = myNormalBorderColor,
